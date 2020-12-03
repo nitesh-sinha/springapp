ab31c418be9bf877659af6806bb22fad29d52ef4
Nitesh Sinha <mit.nitesh@gmail.com>
2020-12-03 23:38:48 +0530

    Used @Qualifier instead of @Primary to specify which is the qualifying Bean amongst two Beans of same type

--- src/main/java/com/niteshphone/Intelprocessor.java
+++ src/main/java/com/niteshphone/Intelprocessor.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary // specifies which is the primary bean of type MobileProcessor
+//@Primary // specifies which is the primary bean of type MobileProcessor
 public class Intelprocessor implements MobileProcessor {
     public void getProcessor() {
         System.out.println("Intel processor selected");
--- src/main/java/com/niteshphone/Samsung.java
+++ src/main/java/com/niteshphone/Samsung.java
@@ -1,11 +1,13 @@
 package com.niteshphone;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Samsung {
     @Autowired
+    @Qualifier("snapdragon")
     MobileProcessor mp;
 
     public MobileProcessor getMp() {
