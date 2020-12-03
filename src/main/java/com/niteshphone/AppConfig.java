fe23e4624e72dd52eac218d6347148239680d238
Nitesh Sinha <mit.nitesh@gmail.com>
2020-12-03 23:31:46 +0530

    Removed @Bean tags(this needs @Component tags in Samsung & Snapdragon clsses) and added @ComponentScan in AppConfig.java

--- src/main/java/com/niteshphone/AppConfig.java
+++ src/main/java/com/niteshphone/AppConfig.java
@@ -1,17 +1,19 @@
 package com.niteshphone;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = "com.niteshphone")
 public class AppConfig {
-    @Bean
-    public Samsung getPhone() {
-        return new Samsung();
-    }
-
-    @Bean
-    public MobileProcessor getMobProcsr() {
-        return new Snapdragon();
-    }
+//    @Bean
+//    public Samsung getPhone() {
+//        return new Samsung();
+//    }
+//
+//    @Bean
+//    public MobileProcessor getMobProcsr() {
+//        return new Snapdragon();
+//    }
 }
--- src/main/java/com/niteshphone/Snapdragon.java
+++ src/main/java/com/niteshphone/Snapdragon.java
@@ -1,5 +1,8 @@
 package com.niteshphone;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Snapdragon implements MobileProcessor{
     public void getProcessor() {
         System.out.println("Snapdragon processor selected");
