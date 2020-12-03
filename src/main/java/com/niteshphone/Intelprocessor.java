b8b572a42bd3e43ef75f52d8169dbaf18c8561fd
Nitesh Sinha <mit.nitesh@gmail.com>
2020-12-03 23:36:06 +0530

    Use of @Primary tag when >1 Beans are of the same type

--- src/main/java/com/niteshphone/Intelprocessor.java
+++ src/main/java/com/niteshphone/Intelprocessor.java
@@ -1,5 +1,10 @@
 package com.niteshphone;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary // specifies which is the primary bean of type MobileProcessor
 public class Intelprocessor implements MobileProcessor {
     public void getProcessor() {
         System.out.println("Intel processor selected");
